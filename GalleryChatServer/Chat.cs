6a1267197d765a41e0d916c3b66699f5a3ccdddc
Neriya Rosner <neriyarosner@gmail.com>
2023-01-27 10:26:37 +0200

    add chat module and controller

---
+++ GalleryChatServer/Chat.cs
@@ -0,0 +1,38 @@
+﻿namespace GalleryChatServer;
+
+public class Chat
+{
+    public int ImageId { get; set; }
+    private List<Message> _Messages = new List<Message>();
+
+    public void AddMessage(Message Message)
+    {
+        this._Messages.Append(Message);
+    }
+
+    public Message[] Messages
+    {
+        get { return _Messages.ToArray(); }
+    }
+}
+
+
+public class Message
+{
+    Message(User Sender, String Text)
+    {
+        this.Sender = Sender;
+        this.Text = Text;
+    }
+
+    public User Sender { get; set; }
+    public string Text { get; set; }
+    public DateTime Date { get; set; } = DateTime.Now;
+}
+
+public class User
+{
+    public int Id { get; set; }
+    public int UserName { get; set; }
+}
+
